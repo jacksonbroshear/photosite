7e7a70eb708a70c20c78ae3a48215f437621b267
Jackson <jackson.plaxis@gmail.com>
2010-04-01 15:44:09 -0700

    removed config.gems from env

--- config/environment.rb
+++ config/environment.rb
@@ -20,12 +20,12 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   # config.gem 'factory_girl'
-  config.gem 'rspec', :lib => false
+  # config.gem 'rspec', :lib => false
   config.gem 'rspec-rails', :lib => false
-  config.gem 'thoughtbot-shoulda',
-              :lib => false,
-              :source => 'http://gems.github.com'
-
+  # config.gem 'thoughtbot-shoulda',
+  #               :lib => false,
+  #               :source => 'http://gems.github.com'
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
